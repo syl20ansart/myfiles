0eac0913eac2e530b5bc1c9fa0f9b42a9e93661d
syl20ansart <84981734+syl20ansart@users.noreply.github.com>
2021-07-13 15:23:44 +0200

    Add files via upload

--- player.js
+++ player.js
@@ -25,7 +25,7 @@ theContext.__proto__.setAdVolume(0.1);
 theContext.setParameter(tv.freewheel.SDK.PARAMETER_USE_GDPR_TCFAPI, true, tv.freewheel.SDK.PARAMETER_LEVEL_GLOBAL);
 theContext.setParameter(tv.freewheel.SDK.PARAMETER_CONSENT_RETRIEVAL_TIMEOUT, 1500, tv.freewheel.SDK.PARAMETER_LEVEL_GLOBAL);
 theContext.addKeyValue('tototo', 'bar');
-theContext.addKeyValue('_fw_vcid2','1213556');
+theContext.addKeyValue('_fw_vcid2','12135562');
 
 theContext.addTemporalSlot("Pre_Roll Standard", tv.freewheel.SDK.ADUNIT_PREROLL, 0);
 
