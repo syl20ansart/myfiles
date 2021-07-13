4f7b859ce1e096405189bab5a0ad96f44ab4253e
syl20ansart <84981734+syl20ansart@users.noreply.github.com>
2021-07-13 15:04:18 +0200

    Add files via upload

--- player.js
+++ player.js
@@ -1,19 +1,15 @@
-var theNetworkId = 516747;
-var theServerURL = "https://7e28b.v.fwmrm.net/ad/g/1";
+var theNetworkId = 57230;
+var theServerURL = "https://df8e.v.fwmrm.net/ad/g/1";
 var theAdManager = new tv.freewheel.SDK.AdManager();
 theAdManager.setNetwork(theNetworkId);
 theAdManager.setServer(theServerURL);
 
-// Working Setup
-var theProfileId = "514966:atres_html5_live" //Your profile name
-//var theVideoAssetId = "7654321"; //Your Video Asset ID
-//var theSiteSectionId  = "TariqSiteSectionLiveTest"; //Your site section ID
 
-//Test Setup
-// var theProfileId = "96749:global-js-byPratik" //Your profile name
-var theVideoAssetId = "The_Flash_S01_E03"; //Your Video Asset ID
-var theSiteSectionId  = "ALPHA_SA_SITESECTION_DESKTOP_1"; //Your site section ID
+var theProfileId = "394455:cpx_test_profile" //Your profile name
 
+var theVideoAssetId = "cpxvideotest"; //Your Video Asset ID
+
+var theSiteSectionId  = "cpxsitess"; //Your site section ID
 
 
 var theVideoDuration = 500;
@@ -21,27 +17,30 @@ theContext = theAdManager.newContext();
 theContext.setProfile(theProfileId);
 theContext.setVideoAsset(theVideoAssetId,theVideoDuration);
 theContext.setSiteSection(theSiteSectionId);
-theContext.__proto__.setAdVolume(0.1);
-theContext.setParameter(tv.freewheel.SDK.PARAMETER_USE_GDPR_TCFAPI, true, tv.freewheel.SDK.PARAMETER_LEVEL_GLOBAL);
-theContext.setParameter(tv.freewheel.SDK.PARAMETER_CONSENT_RETRIEVAL_TIMEOUT, 1500, tv.freewheel.SDK.PARAMETER_LEVEL_GLOBAL);
-theContext.addKeyValue('tototo', 'bar');
-theContext.addKeyValue('_fw_vcid2','121355');
+
 
 theContext.addTemporalSlot("Pre_Roll Standard", tv.freewheel.SDK.ADUNIT_PREROLL, 0);
 
 prerollSlots = [];
 adResponseLoaded = false;
-//videoEl = document.getElementById('video');
-//originalSource=null;
-//
-//
-//// Listen to AdManager Events
+
 theContext.registerVideoDisplayBase("videoBase");
 theContext.addEventListener(tv.freewheel.SDK.EVENT_REQUEST_COMPLETE,onRequestComplete);
 theContext.addEventListener(tv.freewheel.SDK.EVENT_SLOT_STARTED, onSlotStarted);
 theContext.addEventListener(tv.freewheel.SDK.EVENT_SLOT_ENDED,onSlotEnded);
-
+theContext.addEventListener(tv.freewheel.SDK.EVENT_AD, this.onAdEvent.bind(this));
 theContext.setAdVolume(0);
+
+// START CPX JS CODE  
+function onAdEvent(event){
+    if(event.subType === tv.freewheel.SDK.EVENT_AD_CLICK){
+        var adInstance = event.adInstance;
+        adInstance.getRendererController().processEvent({name: tv.freewheel.SDK.EVENT_AD_MEASUREMENT, info:{"concreteEventId": "55"}});
+    }
+}
+// END CPX JS CODE  
+
+
 //// Submit request
 theContext.submitRequest();
 
